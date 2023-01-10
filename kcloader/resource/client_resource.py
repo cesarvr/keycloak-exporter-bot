e215bd31b3dc318c49f8947b121af8ca3ae4afb7
Justin Cinkelj <justin.cinkelj@xlab.si>
2023-01-10 15:54:33 +0100

    Reuse _link_roles
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- kcloader/resource/client_resource.py
+++ kcloader/resource/client_resource.py
@@ -42,50 +42,12 @@ class ClientRoleResource(BaseRoleResource):
             **resource,
         })
 
-    def _link_roles(self):
-        # Get required global knowledge - TODO - move this out, and reuse data, to reduce network traffic
-        realm_roles_api = self.keycloak_api.build("roles", self.realm_name)
-        realm_roles = realm_roles_api.all()
-        clients_api = self.keycloak_api.build("clients", self.realm_name)
-        clients = clients_api.all()
-        roles_by_id_api = self.keycloak_api.build('roles-by-id', self.realm_name)
-
+    def _get_this_role_obj(self, clients, clients_api, realm_roles):
         this_client = find_in_list(clients, clientId=self._client_clientId)
         this_client_roles_api = clients_api.get_child(clients_api, this_client["id"], "roles")
         this_client_roles = this_client_roles_api.all()
         this_role = find_in_list(this_client_roles, name=self.body["name"])
-        this_role_composites_api = roles_by_id_api.get_child(roles_by_id_api, this_role["id"], "composites")
-        # Full role representation will be sent to API (this_role_composites_api) that expects briefRepresentation.
-        # Hopefully this will work.
-
-        # role_obj - object returned by API, role_doc - same data but formatted as if read from json doc
-        this_role_composite_objs = this_role_composites_api.all()
-        this_role_composite_docs = self._get_composites_docs(this_role_composite_objs, clients)
-
-        creation_state = False
-        for sub_role_doc in self.body.get("composites", []):
-            if sub_role_doc in this_role_composite_docs:
-                # link already created
-                # There is nothing that could be updated
-                continue
-            sub_role = find_sub_role(self, clients, realm_roles, clients_roles=None, sub_role=sub_role_doc)
-            if not sub_role:
-                logger.error(f"sub_role {sub_role_doc} not found")
-                # Either ignore or crash
-                # For now, ignore.
-                # TODO - code should crash - on second pass, all subroles should be present.
-                continue
-            this_role_composites_api.create([sub_role]).isOk()
-            creation_state = True
-
-        # remove composites that should not be there
-        for role_obj, role_doc in zip(this_role_composite_objs, this_role_composite_docs):
-            if role_doc not in self.body.get("composites", []):
-                # must be removed
-                this_role_composites_api.remove(None, [role_obj]).isOk()
-                creation_state = True
-
-        return creation_state
+        return this_role
 
 
 class ClientRoleManager(BaseRoleManager):
