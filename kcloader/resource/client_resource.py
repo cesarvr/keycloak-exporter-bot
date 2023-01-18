5be44ebdd99f75a31703ca9a006f6dfcca279941
Justin Cinkelj <justin.cinkelj@xlab.si>
2023-01-18 18:47:52 +0100

    Use ClientScopeMappingsClientManager
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- kcloader/resource/client_resource.py
+++ kcloader/resource/client_resource.py
@@ -6,10 +6,11 @@ from sortedcontainers import SortedDict
 
 import kcapi
 
-from kcloader.resource import SingleResource, ResourcePublisher, UpdatePolicy
+from kcloader.resource import SingleResource
 from kcloader.resource.default_client_scope_resource import ClientOptionalClientScopeManager, ClientDefaultClientScopeManager
 from kcloader.resource.role_resource import find_sub_role, BaseRoleManager, BaseRoleResource
 from kcloader.tools import lookup_child_resource, read_from_json, find_in_list, get_path
+from kcloader.resource.scope_mappings import ClientScopeMappingsAllClientsManager, ClientScopeMappingsRealmManager
 
 logger = logging.getLogger(__name__)
 
@@ -102,40 +103,13 @@ class SingleClientResource(SingleResource):
         self.client_role_manager = None
         self.client_default_client_scope_manager = None
         self.client_optional_client_scope_manager = None
+        self.client__scope_mappings__allclients_manager = None
+        self.client__scope_mappings__realm_manager = None
 
-    def publish_scopes(self):
-        state = True
-        [scopes_path_exist, scopes_path] = lookup_child_resource(self.resource_path, 'scope-mappings.json')
-        if not scopes_path_exist:
-            return state
-        scopes_objects = read_from_json(scopes_path)
-        assert isinstance(scopes_objects, list)
-        if not scopes_objects:
-            # empty list
-            return state
-        assert isinstance(scopes_objects[0], dict)
-
-        clients_api = self.resource.resource_api
-        clients = clients_api.all()
-
-        #  roles_by_id_api.get_child(roles_by_id_api, ci0_default_roles['id'], "composites")
-        this_client = find_in_list(clients, clientId=self.body["clientId"])
-        this_client_scope_mappings_realm_api = clients_api.get_child(clients_api, this_client["id"], "scope-mappings/realm")
-
-        # master_realm = self.keycloak_api.admin()
-        realm_roles_api = self.keycloak_api.build('roles', self.realm_name)
-        realm_roles = realm_roles_api.all()
-
-        # self.keycloak_api.build('clients', self.realm)
-
-        for scopes_object in scopes_objects:
-            role = find_sub_role(self, clients, realm_roles, clients_roles=None, sub_role=scopes_object)
-            if not role:
-                logger.error(f"sub_role {scopes_object} not found")
-            this_client_scope_mappings_realm_api.create([role])
-
-        # TODO remove scope mappings that are assigned, but are not in json file
-        return state
+    def publish_scope_mappings(self):
+        state_realm_roles = self.client__scope_mappings__realm_manager.publish()
+        state_clients_roles = self.client__scope_mappings__allclients_manager.publish()
+        return any([state_realm_roles, state_clients_roles])
 
     def publish_self(self):
         """
@@ -173,16 +147,39 @@ class SingleClientResource(SingleResource):
             client_id=client["id"], client_filepath=self.resource_path,
         )
 
+        # Here we need to compute requested_doc - we cannot just use part of json file.
+        # TODO - kcfethcer should output directly readable json file(s).
+        logger.debug(f"client {self.resource_path}")
+        client_dirname = os.path.dirname(self.resource_path)
+        scope_mappings_filepath = os.path.join(client_dirname, "scope-mappings.json")
+        scope_mappings_doc = read_from_json(scope_mappings_filepath)
+        requested_doc_realm = [rr["name"] for rr in scope_mappings_doc if rr["clientRole"] is False]
+        requested_doc_allclients = {}
+        for rr in scope_mappings_doc:
+            if rr["clientRole"] is False:
+                continue
+            if rr["containerName"] not in requested_doc_allclients:
+                requested_doc_allclients[rr["containerName"]] = []
+            requested_doc_allclients[rr["containerName"]].append(rr["name"])
+        self.client__scope_mappings__realm_manager = ClientScopeMappingsRealmManager(
+            self.keycloak_api, self.realm_name, self.datadir,
+            client_id=client["id"], requested_doc=requested_doc_realm,
+        )
+        self.client__scope_mappings__allclients_manager = ClientScopeMappingsAllClientsManager(
+            self.keycloak_api, self.realm_name, self.datadir,
+            client_id=client["id"], requested_doc=requested_doc_allclients,
+        )
+
         return state
 
     def publish(self, *, include_composite=True):
         state = self.publish_self()
         state_roles = self.client_role_manager.publish(include_composite=include_composite)
-        state_scopes = self.publish_scopes()
+        state_scope_mappings = self.publish_scope_mappings()
         setup_new_links = include_composite
         state_default_client_scopes = self.client_default_client_scope_manager.publish(setup_new_links=setup_new_links)
         state_optional_client_scopes = self.client_optional_client_scope_manager.publish(setup_new_links=setup_new_links)
-        return any([state, state_roles, state_scopes, state_default_client_scopes, state_optional_client_scopes])
+        return any([state, state_roles, state_scope_mappings, state_default_client_scopes, state_optional_client_scopes])
 
     def is_equal(self, obj):
         """
